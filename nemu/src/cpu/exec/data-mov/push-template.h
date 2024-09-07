8ac23a169ff680bb88a1e06a9a931c38f8cd4b6a
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 09:55:54 +0000

    >  compile NEMU 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:55:54 up  4:42,  0 users,  load average: 0.17, 0.08, 0.03 f5289f32ca798d32842b10894cd7e8745894c1

---
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+
+#define instr push
+
+static void do_execute() {
+	cpu.esp -= DATA_BYTE;
+    cpu.esp = MEM_R(REG(ops_decoded.opcode - 0x50));
+	print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
+}
+
+make_instr_helper(i)
+
+#include "cpu/exec/template-end.h"
