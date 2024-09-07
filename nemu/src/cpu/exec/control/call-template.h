713a1a0a143e4d775d12a2c69f4fdce41a5cf619
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 07:39:15 +0000

    >  compile NEMU 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:39:15 up  2:26,  0 users,  load average: 0.14, 0.09, 0.08 2de1f320931bbe1c32b77d18470cf1cec9b0cb3c

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -3,13 +3,35 @@
 #define instr call
 
 static void do_execute() {
-    // TODO: push
     cpu.esp -= DATA_BYTE;
-    swaddr_write(cpu.esp, DATA_BYTE, cpu.eip);
+    MEM_W(cpu.esp, cpu.eip);
     cpu.eip += op_src->val;
     print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
 }
 
 make_instr_helper(i)
+/*
+#if DATA_BYTE == 4
+
+make_helper(call_rm_l) {
+    cpu.esp -= DATA_BYTE;
+    MEM_W(cpu.esp, cpu.eip);
+    int len = decode_rm_l(cpu.eip + 1);
+    cpu.eip = op_src->val;
+    print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
+}
+
+#elif DATA_BYTE == 2
+
+make_helper(call_rm_w) {
+    cpu.esp -= DATA_BYTE;
+    MEM_W(cpu.esp, cpu.eip);
+    int len = decode_rm_w(cpu.eip + 1);
+    cpu.eip = (op_src->val & 0x0000FFFF);
+    print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
+}
+
+#endif
+*/
 
 #include "cpu/exec/template-end.h"
