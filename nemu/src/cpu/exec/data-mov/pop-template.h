30258b256c9ad0bab4b90e9a905fe1a06ba924ea
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-08 05:57:44 +0000

    >  compile NEMU 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:57:44 up  1:13,  0 users,  load average: 0.14, 0.20, 0.09 d51e68daa970aa19463780407513d7638dcfce5e

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,7 +3,7 @@
 #define instr pop
 
 make_helper(concat(pop_, SUFFIX)) {
-    REG(ops_decoded.opcode - 0x50) = MEM_R(cpu.esp);
+    REG(ops_decoded.opcode - 0x58) = MEM_R(cpu.esp);
 	cpu.esp += DATA_BYTE;
 	print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
 	return 1;
