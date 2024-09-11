30b2ff5db5acb7422ca715f1b38918232c785f42
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-11 15:26:27 +0000

    >  compile NEMU 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:26:27 up  5:15,  0 users,  load average: 0.23, 0.14, 0.06 771c53e97f713726305c156e20121157ecb2c7e

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -13,5 +13,20 @@ static void do_execute () {
 }
 
 make_instr_helper(rm)
+#undef instr
+#define instr sete
+
+static void do_execute () {
+	if(cpu.eflags.ZF == 1) {
+        write_operand_b(op_src, 1);
+	}
+	else {
+		write_operand_b(op_src, 0);
+	}
+	print_asm("sete %s", op_src->str);
+}
+
+make_instr_helper(rm)
+#undef instr
 
 #include "cpu/exec/template-end.h"
