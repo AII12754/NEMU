922d2a90a3521a5f8d9fb9ea1c6fe1a3916b9ab4
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-11 11:16:36 +0000

    >  compile NEMU 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:16:36 up  1:05,  0 users,  load average: 0.10, 0.05, 0.01 579ed7452996080d5af5e31a541cf6af16809b4

--- nemu/src/cpu/exec/logic/cmp.c
+++ nemu/src/cpu/exec/logic/cmp.c
@@ -1,5 +1,8 @@
 #include "cpu/exec/helper.h"
 
+#define DATA_BYTE 1
+#include "cmp-template.h"
+#undef DATA_BYTE
 
 #define DATA_BYTE 2
 #include "cmp-template.h"
@@ -11,6 +14,7 @@
 
 /* for instruction encoding overloading */
 
+make_helper_v(cmp_i2a)
 make_helper_v(cmp_i2rm)
 make_helper_v(cmp_si2rm)
 make_helper_v(cmp_r2rm)
