e2d459758aa89006a79d2b81ae1511bf3f868c20
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-12 05:19:02 +0000

    >  run 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:19:02 up  4:42,  0 users,  load average: 0.01, 0.22, 0.19 74e78e1e9238bb811ebf3b043e0f77a1cbe570dd

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,53 +1,79 @@
 #include "FLOAT.h"
-#include <stdint.h>
-
-typedef union {
-	struct {
-		uint32_t m : 23;
-		uint32_t e : 8;
-		uint32_t s : 1;
-	};
-	uint32_t val;
-} Float;
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	long long c = (long long)a * (long long)b;
-	return (FLOAT)(c >> 16);
+	long long ret = 1ll * a * b;
+	return (FLOAT)(ret >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	FLOAT p, q;
-	asm volatile("idiv %2" : "=a"(p), "=d"(q) : "r"(b), "a"(a << 16), "d"(a >> 16));
-	return p;
+	/* Dividing two 64-bit integers needs the support of another library
+	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
+	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
+	 * here. A "64/32" division is enough.
+	 *
+	 * To perform a "64/32" division, you can use the x86 instruction
+	 * `div' or `idiv' by inline assembly. We provide a template for you
+	 * to prevent you from uncessary details.
+	 *
+	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
+	 *
+	 * If you want to use the template above, you should fill the "???"
+	 * correctly. For more information, please read the i386 manual for
+	 * division instructions, and search the Internet about "inline assembly".
+	 * It is OK not to use the template above, but you should figure
+	 * out another way to perform the division.
+	 */
+	int op = 1;
+	if ((a < 0) == 1) op *= -1,a = -a;
+	if ((b < 0) == 1) op *= -1,b = -b;
+
+	int ret = a / b;
+	a %= b;
+
+	int i;
+	for (i = 0;i < 16;i ++){
+		a <<= 1;
+		ret <<= 1;
+		if (a >= b) a -= b, ret |= 1;
+	}
+	return op * ret; 
+
+	
 }
 
 FLOAT f2F(float a) {
+	/* You should figure out how to convert `a' into FLOAT without
+	 * introducing x87 floating point instructions. Else you can
+	 * not run this code in NEMU before implementing x87 floating
+	 * point instructions, which is contrary to our expectation.
+	 *
+	 * Hint: The bit representation of `a' is already on the
+	 * stack. How do you retrieve it to another variable without
+	 * performing arithmetic operations on it directly?
+	 */
+
 	int b = *(int *)&a;
-	int sign = b >> 31;
+	int sign = b & 0x80000000;
 	int exp = (b >> 23) & 0xff;
-	FLOAT c = b & 0x7fffff;
-	if (exp != 0) {
-		c += 1 << 23;
+	int last = b & 0x7fffff;	
+	
+	if(exp == 255) {
+		if (sign) return -0x7fffffff;
+		else return 0x7fffffff;
 	}
-	exp -= 150;
-	if (exp < -16) {
-		c >>= -16 - exp;
-	}
-	if (exp > -16) {
-		c <<= exp + 16;
-	}
-	return sign == 0 ? c : -c;
+	
+	if(exp == 0) return 0;
+
+	last |= 1 << 23;
+	exp -= 134;	
+	if (exp < 0) last >>= -exp;
+	if (exp > 0) last <<= exp;
+
+	if (sign) return -last;else return last;
 }
 
 FLOAT Fabs(FLOAT a) {
-	FLOAT b;
-	if (a > 0){
-		b = a;
-	} else {
-		b = -a;
-
-	}
-	return b;
+	if (a < 0) return -a;else return a;
 }
 
 /* Functions below are already implemented */
@@ -75,3 +101,4 @@ FLOAT pow(FLOAT x, FLOAT y) {
 
 	return t;
 }
+
