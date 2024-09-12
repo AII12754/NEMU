8c2f59d0f16d0a77eb35101cc31ae51ff3ac9d25
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-12 05:43:06 +0000

    >  compile NEMU 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:43:06 up  5:07,  0 users,  load average: 0.15, 0.34, 0.27 b0fc467a950b3a14b13621d5d1afe718af4b545a

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -9,7 +9,7 @@ static void do_execute () {
 	cpu.eflags.CF = result > op_dest->val;
 	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
 
-	Log("%x, %x", cpu.eflags.CF, cpu.eflags.ZF);
+	//Log("%x, %x", cpu.eflags.CF, cpu.eflags.ZF);
 
 	print_asm_template2();
 }
