d61ce9fe669aac2e062b1d8ae5647baf231767cf
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-12 03:49:01 +0000

    >  compile NEMU 3023244162 root Linux 92cac2b289a3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:49:01 up  3:12,  0 users,  load average: 2.54, 2.24, 1.26 597703b2b318f11867e611f7f0147f51e568873

--- nemu/src/cpu/exec/logic/setcc.c
+++ nemu/src/cpu/exec/logic/setcc.c
@@ -1,7 +1,12 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "setcc-template.h"
-#undef DATA_BYTE
+#define make_setcc_helper(cc) \
+	make_helper(concat(set, cc)) { \
+		int len = decode_rm_b(eip + 1); \
+		write_operand_b(op_src, concat(check_cc_, cc)()); \
+		print_asm(str(concat(set, cc)) " %s", op_src->str); \
+		return len + 1; \
+	}
 
-#include "cpu/exec/template-end.h"
+make_setcc_helper(ne)
+make_setcc_helper(e)
