2b7d3a24822d0768e9e973fa24993cdb0d783545
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-28 12:29:45 +0000

    >  compile NEMU 3023244162 root Linux 6aee75527e57 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:29:45 up 57 min,  0 users,  load average: 0.01, 0.04, 0.02

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,7 +10,7 @@ void cache_write(swaddr_t addr, size_t len, uint32_t data);
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	//Log("Hello read 0");
 	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	return cache_read(addr, len);
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
